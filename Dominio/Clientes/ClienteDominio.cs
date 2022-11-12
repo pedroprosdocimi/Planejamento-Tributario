f9c819042771cd8d856c51a9eedf45c8d8ed7434
Pedro Prosdocimi <framework99@localiza.com>
2022-11-12 14:09:36 -0300

    criacao camada de dominio

---
+++ Dominio/Clientes/ClienteDominio.cs
@@ -0,0 +1,39 @@
+﻿using Dominio.Clientes.Interfaces;
+using Entidades;
+using Infra.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dominio.Clientes
+{
+    public class ClienteDominio : IClienteDominio
+    {
+        private IClienteRepositorio _clienteRepositorio;
+
+        public ClienteDominio(IClienteRepositorio clienteRepositorio)
+        {
+            _clienteRepositorio = clienteRepositorio;
+        }
+
+        public async Task AtualizarCliente(Cliente cliente)
+        {
+            await _clienteRepositorio.AtualizarCliente(cliente);
+        }
+
+        public async Task CriarCliente(Cliente cliente)
+        {
+            await _clienteRepositorio.CriarCliente(cliente);
+        }
+
+        public async Task<List<Cliente>> ListarClientes()
+        {
+            return await _clienteRepositorio.ListarCliente();
+        }
+
+        public async Task<Cliente> ObterCliente(Guid idCliente)
+        {
+            return await _clienteRepositorio.ObterCliente(idCliente);   
+        }
+    }
+}
---
+++ Dominio/Clientes/Interfaces/IClienteDominio.cs
@@ -0,0 +1,15 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dominio.Clientes.Interfaces
+{
+    public interface IClienteDominio
+    {
+        Task<Cliente> ObterCliente(Guid idCliente);
+        Task<List<Cliente>> ListarClientes();
+        Task CriarCliente(Cliente cliente);
+        Task AtualizarCliente(Cliente cliente);
+    }
+}
