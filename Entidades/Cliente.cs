67dc6997b894a04389673058abbb412a1878d16f
Pedro Prosdocimi <framework99@localiza.com>
2022-11-12 12:24:07 -0300

    Criando projeto de entidades e adicionando entidade de Cliente

---
+++ Entidades/Cliente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Cliente
+    {
+        public Guid IdClliente { get; set; }    
+        public string NomeCliente { get; set; }
+        public string DocumentoCliente { get; set; }    
+    }
+}
