a242a33799d771a1f4a2e059191e11850a24297b
Pedro Prosdocimi <framework99@localiza.com>
2022-11-12 14:09:14 -0300

    criacao camada de infra

---
+++ Infra/Repositorios/ClienteRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infra.Repositorios
+{
+    public class ClienteRepositorio : IClienteRepositorio
+    {
+        private List<Cliente> ClientesMock;
+        public ClienteRepositorio()
+        {
+            ClientesMock = new List<Cliente>();
+        }
+        public async Task AtualizarCliente(Cliente cliente)
+        {
+            var clienteRemover = ClientesMock.FirstOrDefault(c => c.IdCliente == cliente.IdCliente);
+
+            ClientesMock.Remove(clienteRemover);    
+
+            ClientesMock.Add(cliente);
+        }
+
+        public async Task CriarCliente(Cliente cliente)
+        {
+            ClientesMock.Add(cliente);
+        }
+
+        public async Task<List<Cliente>> ListarCliente()
+        {
+            return ClientesMock;
+        }
+
+        public async Task<Cliente> ObterCliente(Guid IdCliente)
+        {
+            return ClientesMock.FirstOrDefault(c => c.IdCliente == IdCliente);
+        }
+    }
+}
---
+++ Infra/Repositorios/IClienteRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infra.Repositorios
+{
+    public interface IClienteRepositorio
+    {
+        Task<Cliente> ObterCliente(Guid IdCliente);
+        Task<List<Cliente>> ListarCliente();
+        Task CriarCliente(Cliente cliente);
+        Task AtualizarCliente(Cliente cliente); 
+    }
+}
