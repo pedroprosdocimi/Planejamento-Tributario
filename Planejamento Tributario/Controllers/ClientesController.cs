891c87e3e74fca88fe37215c719e8063af8ab348
Pedro Prosdocimi <framework99@localiza.com>
2022-11-12 12:24:41 -0300

    Adicionando referencia do projeto de entidades e criando controller de cliente

---
+++ Planejamento Tributario/Controllers/ClientesController.cs
@@ -0,0 +1,60 @@
+﻿using Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Planejamento_Tributario.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Planejamento_Tributario.Controllers
+{
+    [ApiController]
+    [Route("/clientes")]
+    public class ClientesController : ControllerBase
+    {     
+        private readonly ILogger<ClientesController> _logger;
+
+        public ClientesController(ILogger<ClientesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("/criar")]
+        [ProducesResponseType(typeof(Nullable), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CriarNovoCliente([FromBody] Cliente cliente)
+        {
+                return Ok(new Cliente());
+        }
+
+        [HttpGet]
+        [Route("{idCliente}")]
+        [ProducesResponseType(typeof(Cliente), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Nullable), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> ObterCliente(Guid idCliente)
+        {
+            return Ok(new Cliente());
+        }
+
+        [HttpGet]
+        [Route("")]
+        [ProducesResponseType(typeof(List<Cliente>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> ListarClientes()
+        {
+            return Ok(new Cliente());
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Nullable), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> AtualizarCliente([FromBody] Cliente cliente)
+        {
+            return Ok(new Cliente());
+        }
+    }
+}
